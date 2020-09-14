3e2de069b346b4fe922315fb709965b377d68ed6
adriana correa <luzadriana.ipuz@segurosayg.com>
2020-09-13 23:47:04 -0500

    abrir en inicio

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('layouts/inicio');
 });
 
-Route::get('/login', 'LoginController@index');
+Route::resource('/login', 'LoginController');
 
 //inicio de administrador
 Route::get('/inicio', function () {
