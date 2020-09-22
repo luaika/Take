9b7db6666cfcd4233c4a4c2c2214d5c8bf13b20d
adriana correa <luzadriana.ipuz@segurosayg.com>
2020-09-21 20:13:29 -0500

    estilo

--- resources/js/components/Tercero/registrarTerceros.vue
+++ resources/js/components/Tercero/registrarTerceros.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="container contaRuta">
   <!-- Modal ver -->
-    <div class="modal encabezadoModals fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header  text-white">
+                <div class="modal-header ">
                     <h5 class="modal-title" id="exampleModalLabel">Tercero</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
